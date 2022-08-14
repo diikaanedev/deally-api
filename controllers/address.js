fc0bf2a750a67aff2a9762997a5a4c88df0cc2b9
diii <diikaanedev16@gmail.com>
2022-08-14 13:56:16 +0000

    remplace address to place

--- controllers/address.js
+++ controllers/address.js
@@ -7,30 +7,34 @@ require('dotenv').config({
 
 exports.store = async (req, res, next) => {
 
+    console.log(req.body);
+
 
 
     try {
 
-        let {lastName ,fisrtName , livraisonOrFacture , isMap , phone , zipcode , country , city , addr2 ,  addr1 , name }  = req.body;
+        let { lastName, fisrtName, livraisonOrFacture, isMap, phone, zipcode, country, city, addr2, addr1, name } = req.body;
 
         const address = addressModel();
 
-        address.user_created= req.user.id_user ;
+        console.log(isMap);
 
-        if (isMap!=undefined) {
-            address.isMap = true ;
+        address.user_created = req.user.id_user;
+
+        if (isMap != undefined) {
+            address.isMap = true;
             address.addr1 = addr1;
-        }else {
-            address.lastName = lastName ;
-            address.fisrtName = fisrtName ;
-            address.livraisonOrFacture = livraisonOrFacture ;
-            address.phone = phone ;
-            address.zipcode = zipcode ;
-            address.country = country ;
-            address.city = city ;
-            address.addr2 = addr2 ;
-            address.addr1 = addr1 ;
-            address.name = name ;
+        } else {
+            address.lastName = lastName;
+            address.fisrtName = fisrtName;
+            address.livraisonOrFacture = livraisonOrFacture;
+            address.phone = phone;
+            address.zipcode = zipcode;
+            address.country = country;
+            address.city = city;
+            address.addr2 = addr2;
+            address.addr1 = addr1;
+            address.name = name;
 
         }
 
@@ -38,10 +42,10 @@ exports.store = async (req, res, next) => {
 
         const addressSave = await address.save();
 
-        res.json({
+        return res.json({
             message: ' creation réussi',
             status: 'OK',
-            data:addressSave,
+            data: addressSave,
             statusCode: 201
         });
 
@@ -59,57 +63,57 @@ exports.store = async (req, res, next) => {
 
 exports.update = async (req, res, next) => {
     try {
-        
-        let {lastName ,fisrtName , livraisonOrFacture , isMap , phone , zipcode , country , city , addr2 ,  addr1 , name ,product }  = req.body;
+
+        let { lastName, fisrtName, livraisonOrFacture, isMap, phone, zipcode, country, city, addr2, addr1, name, product } = req.body;
 
         const address = addressModel.findById(req.params.id);
 
 
-        if (fisrtName!=undefined) {
+        if (fisrtName != undefined) {
             address.fisrtName = fisrtName;
         }
 
-        if (lastName!=undefined) {
+        if (lastName != undefined) {
             address.lastName = lastName;
         }
 
-        if (livraisonOrFacture!=undefined) {
+        if (livraisonOrFacture != undefined) {
             address.livraisonOrFacture = livraisonOrFacture;
         }
 
-        if (isMap!=undefined) {
+        if (isMap != undefined) {
             address.isMap = isMap;
         }
 
-        if (phone!=undefined) {
+        if (phone != undefined) {
             address.phone = phone;
         }
 
-        if (zipcode!=undefined) {
+        if (zipcode != undefined) {
             address.zipcode = zipcode;
         }
 
-        if (country!=undefined) {
+        if (country != undefined) {
             address.country = country;
         }
 
-        if (city!=undefined) {
+        if (city != undefined) {
             address.city = city;
         }
 
-        if (addr2!=undefined) {
+        if (addr2 != undefined) {
             address.addr2 = addr2;
         }
 
-        if (addr1!=undefined) {
+        if (addr1 != undefined) {
             address.addr1 = addr1;
         }
 
-        if (name!=undefined) {
+        if (name != undefined) {
             address.name = name;
         }
 
-        if (product!=undefined) {
+        if (product != undefined) {
 
             if (address.prodcuts.includes(product)) {
                 address.prodcuts.push(product);
@@ -121,10 +125,10 @@ exports.update = async (req, res, next) => {
         res.json({
             message: ' mise à jour réussi',
             status: 'OK',
-            data:updateAddress,
+            data: updateAddress,
             statusCode: 201
         });
-        
+
 
     } catch (error) {
         res.json({
@@ -138,41 +142,18 @@ exports.update = async (req, res, next) => {
 }
 
 
-exports.all = async  (req,res,next)=> {
+exports.all = async (req, res, next) => {
 
-   
+
 
     try {
 
         const address = await addressModel.find(req.query).exec();
 
         return res.json({
-             message: ' listage réussi',
-             status: 'OK',
-             data:address,
-             statusCode: 201
-         });
-    } catch (error) {
-        res.json({
-            message: 'erreur mise à jour ',
-            statusCode: 404,
-            data: error,
-            status: 'NOT OK'
-        });
-    }
-}
-
-exports.allUser = async  (req,res,next)=> {
-    try {
-
-        const address = addressModel.find({
-            user_created : req.user.id_users
-        }).excec();
-
-        res.json({
             message: ' listage réussi',
             status: 'OK',
-            data:address,
+            data: address,
             statusCode: 201
         });
     } catch (error) {
@@ -185,7 +166,30 @@ exports.allUser = async  (req,res,next)=> {
     }
 }
 
-exports.one = async  (req,res,next)=> {
+exports.allUser = async (req, res, next) => {
+    try {
+
+        const address = addressModel.find({
+            user_created: req.user.id_users
+        }).excec();
+
+        res.json({
+            message: ' listage réussi',
+            status: 'OK',
+            data: address,
+            statusCode: 201
+        });
+    } catch (error) {
+        res.json({
+            message: 'erreur mise à jour ',
+            statusCode: 404,
+            data: error,
+            status: 'NOT OK'
+        });
+    }
+}
+
+exports.one = async (req, res, next) => {
     try {
 
         const address = addressModel.findOne(req.params.id).excec();
@@ -193,7 +197,7 @@ exports.one = async  (req,res,next)=> {
         res.json({
             message: ' listage réussi',
             status: 'OK',
-            data:address,
+            data: address,
             statusCode: 201
         });
     } catch (error) {
@@ -206,7 +210,7 @@ exports.one = async  (req,res,next)=> {
     }
 }
 
-exports.delete = async  (req,res,next)=> {
+exports.delete = async (req, res, next) => {
     try {
 
         const address = addressModel.findOne(req.params.id).excec();
@@ -216,7 +220,7 @@ exports.delete = async  (req,res,next)=> {
         res.json({
             message: ' supression réussi',
             status: 'OK',
-            data:deleteAddress ,
+            data: deleteAddress,
             statusCode: 201
         });
     } catch (error) {
