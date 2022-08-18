75c3e7e7bfa90a0fcd510139bc2c24aa8f98172b
diii <diikaanedev16@gmail.com>
2022-08-18 16:38:49 +0000

    add order by client

--- routes/order-items.js
+++ routes/order-items.js
@@ -10,6 +10,7 @@ const routes = express.Router();
 // Add routes
 routes.get('/' ,itemOrderCtrl.panierClient);
 routes.get('/orderClient' ,itemOrderCtrl.orderClient);
+routes.get('/:id' ,itemOrderCtrl.one);
 routes.post('/' , itemOrderCtrl.store);
 routes.put('/' , itemOrderCtrl.update);
 routes.delete('/:id' ,itemOrderCtrl.delete);
