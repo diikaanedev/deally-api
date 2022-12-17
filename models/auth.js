d444b824e0fcd1ffa066a95b68850b313a36c9b6
diii <diikaanedev16@gmail.com>
2022-12-17 20:25:42 +0000

    add restaurant role

--- models/auth.js
+++ models/auth.js
@@ -24,7 +24,7 @@ const UserModel = new Schema({
     
     role: {
         type : String,
-        enum: ['admin', 'super', 'fournisseur', 'commercant', 'transporteur','driver', 'logisticien','particulier'],
+        enum: ['admin', 'super', 'fournisseur', 'commercant', 'transporteur','driver', 'logisticien','particulier','restaurant'],
         default: 'commercant'
     },
 
