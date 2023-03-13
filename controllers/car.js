5d56038bcf882449c39bc1374ccef41c7ace659f
diii <diikaanedev16@gmail.com>
2023-03-13 18:02:23 +0000

    update car

--- controllers/car.js
+++ controllers/car.js
@@ -158,128 +158,15 @@ exports.allByUser = async (req,res,next )  => {
 
 exports.update = async (req,res , next) => {
 
-    const  car = await carModel.findById(req.params.id).populate(populateObject).exec();
-
-    console.log(car);
-    console.log(req.params.id);
-
-        const {
-        
-            cover,
-            assuranceFile,
-            licenceFile,
-            categorie,
-            longeur,
-            largeur,
-            hauteur,
-            marque,
-            matricule,
-            model,
-            carteGriseNumber,
-            assuranceNumber,
-            assurannceDate,
-            enLigne
-        }  =  req.body;
-
-        if (cover != undefined) {
-
-            car.cover  =  cover;
-            
-        }
-
-        if (matricule != undefined) {
-
-            car.matricule  =  matricule;
-            
-        }
-
-        if (enLigne != undefined) {
-
-            car.enLigne  =  enLigne;
-            
-        }
-
-        if (assuranceFile != undefined) {
-
-            car.assuranceFile  =  assuranceFile;
-            
-        }
-
-        if (licenceFile != undefined) {
-
-            car.licenceFile  =  licenceFile;
-            
-        }
-
-        if (categorie != undefined) {
-
-            car.categorie  =  categorie;
-            
-        }
-
-        if (longeur != undefined) {
-
-            car.longeur  =  longeur;
-            
-        }
-
-        if (largeur != undefined) {
-
-            car.largeur  =  largeur;
-            
-        }
-
-        if (hauteur != undefined) {
-
-            car.hauteur  =  hauteur;
-            
-        }
-
-        if (marque != undefined) {
-
-            car.marque  =  marque;
-            
-        }
-
-        if (model != undefined) {
-
-            car.model  =  model;
-            
-        }
-
-        if (carteGriseNumber != undefined) {
-
-            car.carteGriseNumber  =  carteGriseNumber;
-            
-        }
-
-        if (assuranceNumber != undefined) {
-
-            car.assuranceNumber  =  assuranceNumber;
-            
-        }
-
-        if (assurannceDate != undefined) {
-
-            car.assurannceDate  =  assurannceDate;
-            
-        }
-
-        const  carSave  = await  car.save();
-
-        const carFind = await  carModel.findById(carSave._id).populate(populateObject).exec();
-
-        return  res.status(200).json({
-            message: 'car update avec success ',
-            status: 'OK',
-            data: carFind,
-            statusCode: 200
-        });
+   
 
 
     try {
 
-        const  car = await carModel.findById(req.query.id).populate(populateObject).exec();
+        const  car = await carModel.findById(req.params.id).populate(populateObject).exec();
+
+    console.log(car);
+    console.log(req.params.id);
 
         const {
         
