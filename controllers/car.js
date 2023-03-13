a749d306e6b8d021c36ba89faa8aa09ca727cb73
diii <diikaanedev16@gmail.com>
2023-03-13 17:42:49 +0000

    update car

--- controllers/car.js
+++ controllers/car.js
@@ -109,7 +109,7 @@ exports.one = async  (req, res , next) => {
 
     try {
 
-        const  carFind = await  carModel.findById(req.query.id).populate(populateObject).exec();
+        const  carFind = await  carModel.findById(req.params.id).populate(populateObject).exec();
         
         return  res.status(200).json({
             message: 'car find avec success ',
@@ -157,6 +157,125 @@ exports.allByUser = async (req,res,next )  => {
 }
 
 exports.update = async (req,res , next) => {
+
+    const  car = await carModel.findById(req.params.id).populate(populateObject).exec();
+
+    console.log(car);
+    console.log(req.params.id);
+
+        const {
+        
+            cover,
+            assuranceFile,
+            licenceFile,
+            categorie,
+            longeur,
+            largeur,
+            hauteur,
+            marque,
+            matricule,
+            model,
+            carteGriseNumber,
+            assuranceNumber,
+            assurannceDate,
+            enLigne
+        }  =  req.body;
+
+        if (cover != undefined) {
+
+            car.cover  =  cover;
+            
+        }
+
+        if (matricule != undefined) {
+
+            car.matricule  =  matricule;
+            
+        }
+
+        if (enLigne != undefined) {
+
+            car.enLigne  =  enLigne;
+            
+        }
+
+        if (assuranceFile != undefined) {
+
+            car.assuranceFile  =  assuranceFile;
+            
+        }
+
+        if (licenceFile != undefined) {
+
+            car.licenceFile  =  licenceFile;
+            
+        }
+
+        if (categorie != undefined) {
+
+            car.categorie  =  categorie;
+            
+        }
+
+        if (longeur != undefined) {
+
+            car.longeur  =  longeur;
+            
+        }
+
+        if (largeur != undefined) {
+
+            car.largeur  =  largeur;
+            
+        }
+
+        if (hauteur != undefined) {
+
+            car.hauteur  =  hauteur;
+            
+        }
+
+        if (marque != undefined) {
+
+            car.marque  =  marque;
+            
+        }
+
+        if (model != undefined) {
+
+            car.model  =  model;
+            
+        }
+
+        if (carteGriseNumber != undefined) {
+
+            car.carteGriseNumber  =  carteGriseNumber;
+            
+        }
+
+        if (assuranceNumber != undefined) {
+
+            car.assuranceNumber  =  assuranceNumber;
+            
+        }
+
+        if (assurannceDate != undefined) {
+
+            car.assurannceDate  =  assurannceDate;
+            
+        }
+
+        const  carSave  = await  car.save();
+
+        const carFind = await  carModel.findById(carSave._id).populate(populateObject).exec();
+
+        return  res.status(200).json({
+            message: 'car update avec success ',
+            status: 'OK',
+            data: carFind,
+            statusCode: 200
+        });
+
 
     try {
 
@@ -290,7 +409,7 @@ exports.update = async (req,res , next) => {
 exports.delete =  async (req,res  ,next)  => {
     try {
         
-        const carFind = await  carModel.findById(req.query.id).populate(populateObject).exec();
+        const carFind = await  carModel.findById(req.params.id).populate(populateObject).exec();
 
         carFind.enLigne = "2";
 
